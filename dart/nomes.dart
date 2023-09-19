f5d88e0c401e180b10d1397c2f0915929c958d82
alice <ajsc1@aluno.ifal.edu.brm>
2023-09-19 16:59:46 -0300

    remove

--- dart/nomes.dart
+++ dart/nomes.dart
@@ -5,7 +5,7 @@ void main() {
 
   while (true) {
     print(
-        "1 - Adicionar nome a lista de nomes\n2- Ver lista atual de nomes\n3- Parar o programa");
+        "1 - Adicionar nome a lista de nomes\n2- Ver lista atual de nomes\n3- remover um nome\n4- Parar o programa");
 
     var opc = int.parse(stdin.readLineSync()!);
 
@@ -21,6 +21,22 @@ void main() {
         }
 
       case 3:
+        print("A lista completa de nomes é:");
+
+        for (int i = 0; i < nomes.length; i++) {
+          print(nomes[i]);
+        }
+
+        print("Qual nome é preciso remover: ");
+        var nome = stdin.readLineSync()!;
+
+        for (int i = 0; i < nomes.length; i++) {
+          if (nome == nomes[i]) {
+            nomes.remove(nome);
+          }
+        }
+
+      case 4:
         return;
     }
   }
