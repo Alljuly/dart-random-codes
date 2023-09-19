642b562c391bf61364be61018f9fbc148390f751
alice <ajsc1@aluno.ifal.edu.brm>
2023-09-19 15:28:54 -0300

    iniciando repositorio do curso de flutter

---
+++ dart/aula1.dart
@@ -0,0 +1,11 @@
+void main() {
+ int idade = 10;
+  
+  if(idade == 16 || idade >= 70){
+    print("Pode votar! (opcional)");
+  } else if(idade < 16){
+    print("Não pode votar!");
+  } else if(idade > 16){
+    print("Pode votar! (obrigatorio)");
+  } 
+}
